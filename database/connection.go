87fb736113afedf4198b065976a6510edeb31223
Anandhu4456 <anandhurajan56@gmail.com>
2023-09-12 20:01:14 +0530

    mongodb connection established

--- database/connection.go
+++ database/connection.go
@@ -1,1 +1,36 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+	MongoDB := os.Getenv("MONGODB_URL")
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := client.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDb...")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"os"
 
-	"go-jwt-model/routers"
+	"go-jwt/routers"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +18,7 @@ func main() {
 	r.Use(gin.Logger())
 
 	routers.AuthRoutes(r)
+	routers.UserRoutes(r)
 	r.GET("/user", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "hey"})
 
